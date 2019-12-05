4f594706503a7b5e7a504c2e80de47cb86e054dc
Oskar Hinc <ohinc@soldevelo.com>
2019-12-05 14:48:32 +0000

    CFLM-374: Added placeholder for the Manage messages page
    Approved-by: Paweł Cieszko <pcieszko@soldevelo.com>

---
+++ owa/app/js/components/patient-template/manage-messages.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { RouteComponentProps } from 'react-router-dom';
+import { IRootState } from '../../reducers';
+import './patient-template.scss';
+
+interface IManageMessagesProps extends DispatchProps, StateProps, RouteComponentProps<{ patientId: string }> {
+};
+
+interface IManageMessagesState {
+};
+
+class ManageMessages extends React.PureComponent<IManageMessagesProps, IManageMessagesState> {
+  render() {
+    const patientId = this.props.match.params.patientId;
+    return (
+      <div className="body-wrapper">
+        <h2>Manage messages</h2>
+        <p>For patient {patientId}</p>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ patientTemplate }: IRootState) => ({
+});
+
+const mapDispatchToProps = ({
+});
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps;
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ManageMessages);
--- owa/app/js/components/patient-template/routes.tsx
+++ owa/app/js/components/patient-template/routes.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom';
 import PatientTemplateEdit from './patient-template-edit';
+import ManageMessages from './manage-messages';
 
 const Routes = (props) => (
   <>
     <Switch>
       <Route
-        path={`${props.match.url}/new/:patientId`}
+        path={`${props.match.url}/:patientId/new`}
         render={(props) => <PatientTemplateEdit {...props} isNew={true} />}
       />
       <Route
-        path={`${props.match.url}/edit/:patientId`}
+        path={`${props.match.url}/:patientId/edit`}
         render={(props) => <PatientTemplateEdit {...props} isNew={false} />}
+      />
+      <Route
+        path={`${props.match.url}/:patientId`}
+        render={(props) => <ManageMessages {...props} />}
       />
     </Switch>
   </>
