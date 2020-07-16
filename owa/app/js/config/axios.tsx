cf6d516883a8b6124615abbc700d6b28d21278de
Arkadiusz Lalo <alalo@soldevelo.com>
2020-07-16 13:20:29 +0000

    CFLM-1168: Added redirect on 401
    Approved-by: Kamil Madej <kmadej@soldevelo.com>

--- owa/app/js/config/axios.tsx
+++ owa/app/js/config/axios.tsx
@@ -1,15 +1,30 @@
 import axios from 'axios';
+import { loginActions } from '@openmrs/react-components';
 
 // According to OpenMRS convention, setting page to 0 will result in returning all records
 export const ALL_RECORDS_PAGE = 0;
 
-const apiBaseUrl = '/openmrs/';
+const getApiBaseUrl = () => {
+  const path = window.location.pathname;
+  return path.substring(0, path.indexOf('/owa/')) + '/';
+}
 
 const axiosInstance = axios.create({
-  baseURL: apiBaseUrl,
+  baseURL: getApiBaseUrl(),
   headers: {
     accept: 'application/json',
   },
+});
+
+axiosInstance.interceptors.response.use(function (response) {
+  return response;
+}, function (error) {
+  if (401 === error.response.status) {
+    loginActions.logout();
+    window.location.href = getApiBaseUrl() + '/login.htm?redirectUrl=' + window.location.href
+  } else {
+      return Promise.reject(error);
+  }
 });
 
 export default axiosInstance;
