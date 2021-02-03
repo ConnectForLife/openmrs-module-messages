0c72e5ad3e15ecec12e826675710c3bf838bc961
Dawid Ruchniewicz <druchniewicz@soldevelo.com>
2021-02-03 12:20:51 +0000

    Merged in feature/CFLM-1535 (pull request #362)
    CFLM-1535: Encoded url to avoid injection of special characters
    
    * CFLM-1535: Encoded url to avoid injection of special characters
    
    * CFLM-1535: Checked correctness of urls before redirecting
    
    Approved-by: Paweł Gesek

--- owa/app/js/config/axios.tsx
+++ owa/app/js/config/axios.tsx
@@ -3,10 +3,13 @@ import { loginActions } from '@openmrs/react-components';
 
 // According to OpenMRS convention, setting page to 0 will result in returning all records
 export const ALL_RECORDS_PAGE = 0;
+export const OPENMRS_CONTEXT_PATH = '/openmrs';
+export const DOMAIN_PATH = window.location.protocol + '//' + window.location.host + OPENMRS_CONTEXT_PATH;
+export const REDIRECT_URL = window.location.href;
 
 const getApiBaseUrl = () => {
   const path = window.location.pathname;
-  return path.substring(0, path.indexOf('/owa/')) + '/';
+  return path.substring(0, path.indexOf('/owa/'));
 }
 
 const axiosInstance = axios.create({
@@ -16,12 +19,19 @@ const axiosInstance = axios.create({
   },
 });
 
+const isValidUrl = (url: string) => {
+  return url.startsWith(DOMAIN_PATH) && url.indexOf(OPENMRS_CONTEXT_PATH) !== -1;
+}
+
 axiosInstance.interceptors.response.use(function (response) {
   return response;
 }, function (error) {
-  if (401 === error.response.status) {
-    loginActions.logout();
-    window.location.href = getApiBaseUrl() + '/login.htm?redirectUrl=' + window.location.href
+    const isValidRedirectUrl = isValidUrl(REDIRECT_URL);
+    const fullUrlAddress = DOMAIN_PATH + '/login.htm?redirectUrl=' + REDIRECT_URL;
+    const isValidFullUrl = isValidUrl(fullUrlAddress);
+    if (401 === error.response.status && isValidRedirectUrl && isValidFullUrl) {
+      loginActions.logout();
+      window.location.href = fullUrlAddress;
   } else {
       return Promise.reject(error);
   }
