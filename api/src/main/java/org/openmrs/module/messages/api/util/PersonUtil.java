517ca7e101ee11eb1e235ebb1265a3d5745fe2a4
druchniewicz <druchniewicz@soldevelo.com>
2021-08-06 14:48:41 +0200

    AGRE-1737: Used StringJoiner to strings concatenation

--- api/src/main/java/org/openmrs/module/messages/api/util/PersonUtil.java
+++ api/src/main/java/org/openmrs/module/messages/api/util/PersonUtil.java
@@ -3,21 +3,23 @@ package org.openmrs.module.messages.api.util;
 import org.apache.commons.lang.StringUtils;
 import org.openmrs.Person;
 
+import java.util.StringJoiner;
+
 public final class PersonUtil {
 
     public static String getPersonFullName(Person person) {
-        StringBuilder sb = new StringBuilder();
+        StringJoiner sj = new StringJoiner(" ");
         if (StringUtils.isNotBlank(person.getGivenName())) {
-            sb.append(person.getGivenName()).append(" ");
+            sj.add(person.getGivenName());
         }
         if (StringUtils.isNotBlank(person.getMiddleName())) {
-            sb.append(person.getMiddleName()).append(" ");
+            sj.add(person.getMiddleName());
         }
         if (StringUtils.isNotBlank(person.getFamilyName())) {
-            sb.append(person.getFamilyName());
+            sj.add(person.getFamilyName());
         }
 
-        return sb.toString().trim();
+        return sj.toString();
     }
 
     private PersonUtil() {
